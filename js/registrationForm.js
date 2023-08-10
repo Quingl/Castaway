09b96bc30aa647044a9a7f97b628a433ead3f003
Quingl <dandov@inbox.ru>
2023-08-10 12:12:54 +0300

    refactoring registrationForm

--- js/registrationForm.js
+++ js/registrationForm.js
@@ -1,32 +1,37 @@
-function toggleLoader() {
-    const loader = document.getElementById('loader')
-    loader.classList.toggle('hidden')
+  function toggleLoader() {
+    const loader = document.getElementById('loader');
+    loader.classList.toggle('hidden');
   }
 
   function onError(error) {
-    alert(error.message)
+    alert(error.message);
   }
 
   function checkValidity(event) {
-    const formNode = event.target.form
-    const isValid = formNode.checkValidity()
+    const formNode = event.target.form;
+    const isValid = formNode.checkValidity();
   
-    formNode.querySelector('button').disabled = !isValid
+    formNode.querySelector('button').disabled = !isValid;
   }
 
-function onSuccess(formNode) {
-    alert('Ваша заявка отправлена!')
-    formNode.classList.toggle('hidden')
+  function onSuccess(formNode) {
+    alert('Ваша заявка отправлена!');
+    formNode.classList.toggle('hidden');
   }
 
 
-function serializeForm(formNode) {
+
+  function serializeForm(formNode) {
     let data = new FormData(formNode);
-    console.log(Array.from(data.entries()))
+
+    console.log(Array.from(data.entries()));
+
     var object = {};
+
     data.forEach(function(value, key){ object[key] = value; });
     console.log(JSON.stringify(object));
     data = JSON.stringify(object);
+
     return data;
   }
 
@@ -38,28 +43,25 @@ function serializeForm(formNode) {
     })
   }
 
-
-  
-
-async function handleFormSubmit(event) {
+  async function handleFormSubmit(event) {
     event.preventDefault();
     
     let data = serializeForm(event.target);
+
     toggleLoader();
-    console.log(data);
     const {status} = await sendData(data);
     toggleLoader();
 
 
 
     if (status === 200) {
-        onSuccess(event.target);
+      onSuccess(event.target);
   } else {
-    console.log(status);
     onError(error);
   }
 }
   const applicantForm = document.getElementById('grade-form');
+  
   applicantForm.addEventListener('submit', handleFormSubmit);
   applicantForm.addEventListener('input', checkValidity);
   
